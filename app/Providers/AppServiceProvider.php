73f9804d196a872ab3057669a33758b62c407385
solomon chigozie <solomon1chigozie@gmail.com>
2021-11-25 00:52:02 +0100

    new commit

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,8 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Schema;
+use Log;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -24,8 +26,10 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         //
-        if(config('app.env') === 'production') {
-            \URL::forceScheme('https');
+        If (env('APP_ENV') !== 'local') {
+            $this->app['request']->server->set('HTTPS', true);
         }
+
+        Schema::defaultStringLength(191);
     }
 }
